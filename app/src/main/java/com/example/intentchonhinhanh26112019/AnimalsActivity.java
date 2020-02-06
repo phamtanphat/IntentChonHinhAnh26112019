5faebce0b5193d3f2391f15009ce3e0aed9dc8a4
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-02-06 18:54:44 +0700

    find id image

--- app/src/main/java/com/example/intentchonhinhanh26112019/AnimalsActivity.java
+++ app/src/main/java/com/example/intentchonhinhanh26112019/AnimalsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -11,24 +12,35 @@ import android.widget.TableRow;
 public class AnimalsActivity extends AppCompatActivity {
 
     TableLayout mTableLayout;
+    String[] arrayNamesAnimal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals);
+        init();
         mapview();
         drawTableImage();
+    }
+
+    private void init() {
+        arrayNamesAnimal = getResources().getStringArray(R.array.arrayNamesAnimal);
     }
 
     private void drawTableImage() {
         int socot = 3;
         int sodong = 6;
+        int index = 0;
         for (int i = 0 ; i < sodong ; i++){
             TableRow tableRow = new TableRow(AnimalsActivity.this);
             for (int y = 0 ; y < socot ; y++){
                 ImageView imageView = new ImageView(AnimalsActivity.this);
                 TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(355,350);
                 imageView.setLayoutParams(layoutParams);
-                imageView.setImageResource(R.drawable.bo);
+                int valueHinhChon = getResources().getIdentifier(
+                        arrayNamesAnimal[index++],
+                        "drawable",
+                        getPackageName());
+                imageView.setImageResource(valueHinhChon);
                 tableRow.addView(imageView);
             }
             mTableLayout.addView(tableRow);
